982bd76728627eadefc36128c37351e6fb22fc10
SubrahmanyaG <subramanya.g@hashedin.com>
2017-10-03 19:34:36 +0530

    fixed image dispaly issue

--- user_details/admin.py
+++ user_details/admin.py
@@ -9,7 +9,7 @@ class UserDetailsAdmin(admin.ModelAdmin):
     list_filter = ['state', 'person', 'created_at']
 
     def display_photo(self, obj):
-            return '<img src="/static/{}" height="150", width="150" alt="Image">'.format(obj.image)
+        return '<img src="/static/{}" height="150", width="150" alt="Image">'.format(obj.image.name.split("/")[-1])
 
     display_photo.allow_tags = True
 admin.site.register(UserDetails, UserDetailsAdmin)
