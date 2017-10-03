605059bf81517bbaa245b1b34c30f6d417c9a5e4
SubrahmanyaG <subramanya.g@hashedin.com>
2017-10-03 16:05:17 +0530

    added filter to the admin page to display user details based on state, created date, name

--- user_details/admin.py
+++ user_details/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import  UserDetails
 # Register your models here.
-admin.site.register(UserDetails)
+
+
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ['person', ]
+    list_filter= ['state', 'person', 'created_at']
+admin.site.register(UserDetails, UserDetailsAdmin)
